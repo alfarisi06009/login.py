fe1290852b611fd557b3d29ec20790b0d97c104e
alfarisi06009 <72635332+alfarisi06009@users.noreply.github.com>
2022-05-15 20:27:26 +0700

    Add files via upload

---
+++ login.py
@@ -0,0 +1,64 @@
+#python
+#login Script
+
+from os import access, name
+from typing import Optional
+
+
+def login(name,password):
+    sukses = False
+    file = open("login.txt", "r")
+    for i in file:
+        a,b = i.split(",")
+        b = b.strip()
+        if(a==name and b==password):
+            sukses = True
+            break
+    file.close()
+    if(sukses):
+        print("******************************")
+        print("login berhasil, silahkan masuk")
+        print("******************************")
+    else:
+        print("***************************************")
+        print("anda belum terdaftar, silahkan register")
+        print("***************************************")
+
+def register(name,password):
+    file = open("login.txt", "a")
+    file.write("\n"+name+","+password)
+
+def access(option):
+    global name
+    if(option == "login"):
+        name = input("masukan ID : ")
+        Password = input("masukan password : ")
+        login(name,Password)
+    else:
+        print("Masukan ID dan password anda yang baru")
+        name = input("masukan ID : ")
+        Password = input("masukan password : ")
+        register(name,Password)
+        print("register anda berhasil, silahkan masuk")
+
+#tampilan
+
+def begin():
+    global option
+    print("SAFARI SC")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print("[+] Author : Al Farisi                        [+]")
+    print("[+] Team   : Safari Creative                  [+]")
+    print("[+] Github : https://github.com/alfarisi06009 [+]")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print("Gratis tanpa bayaran kok")
+    print("========================================")
+    print("ketik 'login' jika anda sudah punya akun")
+    print("ketik 'reg' jika anda belum punya akun")
+    print("========================================")
+    option = input("silahkan masukan (login/reg) : ")
+    if(option!="login" and option!="reg"):
+        begin()
+
+begin()
+access(option)
